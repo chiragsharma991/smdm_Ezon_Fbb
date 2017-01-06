23becf9094f9a7d029ae68b70e2dd4998f6f5cdd
csuthar <charisharma16@gmail.com>
2017-01-06 20:15:24 +0530

    1. Again Recheck all labels and values from Api one by one in Inventory Module.

---
+++ app/src/main/java/apsupportapp/aperotechnologies/com/designapp/model/SkewedSizeListDisplay.java
@@ -0,0 +1,98 @@
+package apsupportapp.aperotechnologies.com.designapp.model;
+
+/**
+ * Created by csuthar on 06/01/17.
+ */
+
+public class SkewedSizeListDisplay
+{
+    String storeCode;
+    String storeDescription;
+    String option;
+
+    public String getStoreCode() {
+        return storeCode;
+    }
+
+    public void setStoreCode(String storeCode) {
+        this.storeCode = storeCode;
+    }
+
+    public String getStoreDescription() {
+        return storeDescription;
+    }
+
+    public void setStoreDescription(String storeDescription) {
+        this.storeDescription = storeDescription;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+
+    public String getProdAttribute4() {
+        return prodAttribute4;
+    }
+
+    public void setProdAttribute4(String prodAttribute4) {
+        this.prodAttribute4 = prodAttribute4;
+    }
+
+    public String getProdImageURL() {
+        return prodImageURL;
+    }
+
+    public void setProdImageURL(String prodImageURL) {
+        this.prodImageURL = prodImageURL;
+    }
+
+    public String getStkOnhandQty() {
+        return stkOnhandQty;
+    }
+
+    public void setStkOnhandQty(String stkOnhandQty) {
+        this.stkOnhandQty = stkOnhandQty;
+    }
+
+    public String getFwdWeekCover() {
+        return fwdWeekCover;
+    }
+
+    public void setFwdWeekCover(String fwdWeekCover) {
+        this.fwdWeekCover = fwdWeekCover;
+    }
+
+    public String getSkewedFlag() {
+        return skewedFlag;
+    }
+
+    public void setSkewedFlag(String skewedFlag) {
+        this.skewedFlag = skewedFlag;
+    }
+
+    public int getSkewedFlagCount() {
+        return skewedFlagCount;
+    }
+
+    public void setSkewedFlagCount(int skewedFlagCount) {
+        this.skewedFlagCount = skewedFlagCount;
+    }
+
+    String prodAttribute4;
+    String prodImageURL;
+    String stkOnhandQty;
+    String fwdWeekCover;
+    String skewedFlag;
+    int skewedFlagCount;
+
+
+
+
+
+
+
+}
