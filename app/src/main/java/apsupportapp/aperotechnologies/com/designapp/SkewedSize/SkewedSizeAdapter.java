6e124bb77b37cd0a1e4f87d1f12cde827912de39
csuthar <charisharma16@gmail.com>
2017-01-09 15:03:03 +0530

    1. Change skewed size activity and functionality.

--- app/src/main/java/apsupportapp/aperotechnologies/com/designapp/SkewedSize/SkewedSizeAdapter.java
+++ app/src/main/java/apsupportapp/aperotechnologies/com/designapp/SkewedSize/SkewedSizeAdapter.java
@@ -5,14 +5,19 @@ package apsupportapp.aperotechnologies.com.designapp.SkewedSize;
  */
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -43,6 +48,13 @@ public class SkewedSizeAdapter extends BaseAdapter {
     Context context;
     private int Position;
     String TAG="SkewedSizesActivity";
+    private TextView mType;
+    private List<String> product;
+    private Holder holder;
+    private List<String> SOH;
+    private List<String> items;
+    private List<String> setFlag;
+    private List<String> setFwc;
 
     //private ValueFilter valueFilter;
 
@@ -85,7 +97,7 @@ public class SkewedSizeAdapter extends BaseAdapter {
         //Log.e("in ","getview");
 
         Position = position;
-        final Holder holder;
+
         if (convertView == null) {
             holder = new Holder();
             convertView = mInflater.inflate(R.layout.skewedsize_child, null);
@@ -93,11 +105,14 @@ public class SkewedSizeAdapter extends BaseAdapter {
             holder.ProgressPicaso = (ProgressBar) convertView.findViewById(R.id.progressPicaso);
             holder.ProgressPicaso.setVisibility(View.VISIBLE);
             holder.skewed_fwc = (TextView) convertView.findViewById(R.id.skewed_fwc);
-            holder.Skewed_ProdAttribute = (TextView) convertView.findViewById(R.id.skewed_ProdAttribute);
-            holder.Skewed_SOH = (TextView) convertView.findViewById(R.id.skewed_SOH);
+            // holder.Skewed_ProdAttribute = (TextView) convertView.findViewById(R.id.skewed_ProdAttribute);
+            // holder.Skewed_SOH = (TextView) convertView.findViewById(R.id.skewed_SOH);
             holder.skewed_option = (TextView) convertView.findViewById(R.id.skewed_option);
             holder.skewed_image_child = (ImageView) convertView.findViewById(R.id.skewed_image_child);
             holder.toggle_skewed_fav = (ToggleButton) convertView.findViewById(R.id.toggle_skewed_fav);
+            holder.SOH = (LinearLayout) convertView.findViewById(R.id.sohDetails);
+            holder.ProductAttribute = (LinearLayout) convertView.findViewById(R.id.productInfo);
+
 
 
             convertView.setTag(holder);
@@ -109,10 +124,33 @@ public class SkewedSizeAdapter extends BaseAdapter {
 
         }
         holder.skewed_option.setText(arrayList.get(position).getOption());
-       // holder.skewed_SOHU.setText(calculation(arrayList.get(position).getStkOnhandQty()));
-        holder.skewed_fwc.setText(arrayList.get(position).getFwdWeekCover());
-       // holder.Skewed_ProdAttribute.setText(arrayList.get(position).getProdAttribute4());
-       // holder.Skewed_SOH.setText((arrayList.get(position).getStkOnhandQty()));
+        holder.skewed_SOHU.setText(""+calculation(arrayList.get(position).getStkOnhandQty()));
+        product=new ArrayList<String>();
+        product.clear();
+        product = Arrays.asList(arrayList.get(position).getProdAttribute4().split("\\s*,\\s*"));
+        setFlag = Arrays.asList(arrayList.get(position).getSkewedFlag().split("\\s*,\\s*"));
+        setFwc = Arrays.asList(arrayList.get(position).getFwdWeekCover().split("\\s*,\\s*"));
+       // ArrayList<String>setNewFwc=new ArrayList<String>();
+        StringBuilder stringBuild=new StringBuilder();
+        for (int i = 0; i <setFwc.size() ; i++) {
+            Double x= Double.parseDouble(setFwc.get(i));
+            //setNewFwc.add(String.format("%.1f",x));
+            stringBuild.append(String.format("%.1f",x));
+            stringBuild.append(",");
+
+        }
+        stringBuild.deleteCharAt(stringBuild.length()-1);
+        holder.skewed_fwc.setText(stringBuild);
+
+        createText();
+        createSOH();
+
+
+
+
+
+        // holder.Skewed_ProdAttribute.setText(arrayList.get(position).getProdAttribute4());
+        // holder.Skewed_SOH.setText((arrayList.get(position).getStkOnhandQty()));
         Log.e(TAG, "getView: "+calculation(arrayList.get(position).getStkOnhandQty()));
 
 
@@ -148,9 +186,88 @@ public class SkewedSizeAdapter extends BaseAdapter {
         return convertView;
     }
 
+    private void createSOH()
+    {
+        holder.SOH.removeAllViewsInLayout();
+
+        for (int i = 0; i <items.size() ; i++) {
+
+            mType = new TextView(context);
+
+            mType.setLayoutParams(new LinearLayout.LayoutParams(70,30));
+
+
+//            mType.setLayoutParams(new LinearLayout.LayoutParams(
+//                    LinearLayout.LayoutParams.WRAP_CONTENT,
+//                    LinearLayout.LayoutParams.WRAP_CONTENT,1f));
+
+
+            if(setFlag.get(i).equals("Y")){
+                mType.setBackgroundResource(R.drawable.cell_shape_mark);
+                mType.setTextColor(Color.parseColor("#ffffff"));
+
+
+            }else
+            {
+                mType.setBackgroundResource(R.drawable.cell_shape);
+                mType.setTextColor(Color.parseColor("#404040"));
+
+            }
+            mType.setTextSize(12);
+            mType.setGravity(Gravity.CENTER);
+            mType.setText(""+items.get(i));
+
+
+            holder.SOH.addView(mType);
+
+        }
+    }
+
+    private void createText()
+    {
+        holder.ProductAttribute.removeAllViewsInLayout();
+
+        for (int i = 0; i <product.size() ; i++) {
+
+            mType = new TextView(context);
+
+
+            mType.setLayoutParams(new LinearLayout.LayoutParams(70,30));
+
+
+            //       mType.setPadding(5, 3, 0, 3);
+            if(setFlag.get(i).equals("Y")){
+                mType.setBackgroundResource(R.drawable.cell_shape_mark);
+                mType.setTextColor(Color.parseColor("#ffffff"));
+
+
+            }else
+            {
+                mType.setBackgroundResource(R.drawable.cell_shape);
+                mType.setTextColor(Color.parseColor("#404040"));
+
+            }
+            mType.setTextSize(12);
+            mType.setGravity(Gravity.CENTER);
+            mType.setText(""+product.get(i));
+            mType.setTypeface(null, Typeface.BOLD);
+
+            holder.ProductAttribute.addView(mType);
+
+
+
+
+        }
+
+
+
+
+
+    }
+
 
     private int calculation(String value) {
-        List<String> items = Arrays.asList(value.split("\\s*,\\s*"));
+        items = Arrays.asList(value.split("\\s*,\\s*"));
         int stkOnhandQty []=new int[items.size()];
         for (int i = 0; i <items.size(); i++) {
             stkOnhandQty[i]=Integer.parseInt(items.get(i));
@@ -168,7 +285,7 @@ public class SkewedSizeAdapter extends BaseAdapter {
             sum += y;
         }
         return sum;
-       // Log.e(TAG, "sumof: "+sum );
+        // Log.e(TAG, "sumof: "+sum );
     }
 
 
@@ -177,6 +294,7 @@ public class SkewedSizeAdapter extends BaseAdapter {
         TextView skewed_SOHU, skewed_fwc, skewed_option, Skewed_ProdAttribute, Skewed_SOH;
         ImageView skewed_image_child;
         ToggleButton toggle_skewed_fav;
+        LinearLayout ProductAttribute ,SOH;
         public ProgressBar ProgressPicaso;
     }
 }
