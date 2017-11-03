fd3423833fe415b69436811591b8692c97e525d9
rkanawade <rkanawade@aperotechnologies.com>
2017-11-03 12:43:55 +0530

    Created HomeActivity screen in BigBazaar module

--- app/src/main/java/apsupportapp/aperotechnologies/com/designapp/BigBazaar/HomeActivity.java
+++ app/src/main/java/apsupportapp/aperotechnologies/com/designapp/BigBazaar/HomeActivity.java
@@ -1,8 +1,98 @@
 package apsupportapp.aperotechnologies.com.designapp.BigBazaar;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.RelativeLayout;
+
+import apsupportapp.aperotechnologies.com.designapp.FeedbackofCustomer.AvailabilityAndNotifyHO.Adapter.ProductAvailability_ViewPagerAdapter;
+import apsupportapp.aperotechnologies.com.designapp.R;
+
 /**
  * Created by pamrutkar on 01/11/17.
  */
 
-public class HomeActivity {
+public class HomeActivity extends AppCompatActivity{
+
+    TabLayout tabLayout;
+    private RelativeLayout imageBtnBack1, btn_Search;
+    private ViewPager viewPager;
+    private Context context;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_bigbazar);
+
+//        getSupportActionBar().hide();
+//        getSupportActionBar().setElevation(0);
+        context = this;
+        imageBtnBack1 = (RelativeLayout) findViewById(R.id.imageBtnBack1);
+        btn_Search = (RelativeLayout) findViewById(R.id.btn_Search);
+
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Home"));
+        tabLayout.addTab(tabLayout.newTab().setText("Categories"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#e8112d"));
+
+        imageBtnBack1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        final HomeAdapter adapter = new HomeAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                View focus = getCurrentFocus();
+                if (focus != null) {
+                    hiddenKeyboard(focus);
+                }
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+
+            }
+        });
+
+        btn_Search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    private void hiddenKeyboard(View v) {
+        InputMethodManager keyboard = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        keyboard.hideSoftInputFromWindow(v.getWindowToken(), 0);
+    }
+
+
 }
